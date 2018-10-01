b88576fe100b9263e1885ecb459b8fc7d448eeb8
Adriano Araujo <aadriano990@gmail.com>
2018-10-01 17:10:51 -0300

    Tratando erro 403

--- src/interceptors/error.interceptor.ts
+++ src/interceptors/error.interceptor.ts
@@ -1,15 +1,17 @@
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
+import { StoregeService } from "../services/estorege.service";
+import { Injectable } from "@angular/core";
 
-
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
     
-
-
+    constructor(public storege: StoregeService){
+    }
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log("Passou");
         return next.handle(req)
-        .catch((error, caught) =>{
+        .catch((error, caught) => {
             let errorObj = error;
             if(errorObj.error){
                 errorObj = error;
@@ -17,14 +19,19 @@ export class ErrorInterceptor implements HttpInterceptor{
                 if(!errorObj.status){
                     errorObj = JSON.parse(errorObj);
                 }
-
                 console.log("Error detectado pelo interceptor");
                 console.log(errorObj);
-
+                switch(errorObj.status) {
+                    case 403:
+                    this.handle403();
+                    break;
+                }
             return Observable.throw(errorObj);
         }) as any;
     }
-
+    handle403() {
+        this.storege.setLocalUser(null);
+  }
 }
 
  export const ErrorInterceptorProvides = {
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -32,7 +32,14 @@ export class ProfilePage {
           this.cliente = response;
           this.getImageIfExiste();
         },
-        error => {});
+        error => {
+          if(error.status == 403){
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
+    } 
+    else{
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
