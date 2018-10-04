97d14bda8e955a3101bc3ed99465e06c0122d129
Adriano Araujo <aadriano990@gmail.com>
2018-10-04 17:43:29 -0300

    Salvando novo cliente

--- src/interceptors/error.interceptor.ts
+++ src/interceptors/error.interceptor.ts
@@ -3,6 +3,7 @@ import { Observable } from "rxjs/Rx";
 import { StoregeService } from "../services/estorege.service";
 import { Injectable } from "@angular/core";
 import { AlertController} from "ionic-angular";
+import { FieldMessage } from "../models/filedmessage.dto";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
@@ -30,6 +31,10 @@ export class ErrorInterceptor implements HttpInterceptor{
                     case 403:
                     this.handle403();
                     break;
+                    
+                    case 422:
+                    this.handle422(errorObj);
+                    break;
 
                     default:
                     this.handleDefaultError(errorObj);
@@ -55,6 +60,21 @@ export class ErrorInterceptor implements HttpInterceptor{
     handle403() {
         this.storege.setLocalUser(null);
   }
+
+  handle422(errorObj){
+    let alert = this.alertCtrl.create({
+        title: 'Erro de validação',
+        message: this.listErrors(errorObj.errors),
+        enableBackdropDismiss: false,
+        buttons : [
+            {
+                text: 'Ok'
+            }
+        ]
+    });
+    alert.present();
+}
+
   handleDefaultError(errorObj){
     let alert = this.alertCtrl.create({
         title: 'Erro ' + errorObj.status  + ': ' + errorObj.error,
@@ -68,6 +88,15 @@ export class ErrorInterceptor implements HttpInterceptor{
     });
     alert.present();
   }
+  
+private listErrors(messages: FieldMessage[]):string {
+    let s : string = '';
+    for(var i=0;i<messages.length;i++){
+        s = s + '<p><strong>' + messages[i].fieldName + "</strong>:" + messages[i].message + '</p>';
+    }
+    return s;
+}
+
 }
 
  export const ErrorInterceptorProvides = {
--- src/pages/singup/singup.ts
+++ src/pages/singup/singup.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { EstadoService } from '../../services/domain/estado.service';
 import { CidadeService } from '../../services/domain/cidade.service';
 import { EstadoDTO } from '../../models/estado.dto';
 import { CidadeDTO } from '../../models/cidade.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
 
 /**
  * Generated class for the SingupPage page.
@@ -28,20 +29,22 @@ export class SingupPage {
      public navParams: NavParams, 
      public formBuilder: FormBuilder,
      public estadoService: EstadoService,
-     public cidadeService: CidadeService) {
+     public cidadeService: CidadeService,
+     public clienteService: ClienteService,
+     public alertCtrl: AlertController) {
 
     this.formGroup = this.formBuilder.group({
-    nome : ['',[Validators.required, Validators.minLength(5),Validators.maxLength(100)]],
-    email: ['',[Validators.required, Validators.email ]],
-    tipo: ['', [Validators.required]],
-    cpfOuCnpj: ['',[Validators.required,Validators.minLength(11),Validators.maxLength(14)]],
-    senha: ['',[Validators.required]],
-    logradouro: [' ',[Validators.required]],
-    numero: ['',[]],
+    nome : ['Joana',[Validators.required, Validators.minLength(5),Validators.maxLength(100)]],
+    email: ['Joana@gmail.com',[Validators.required, Validators.email ]],
+    tipo: ['1', [Validators.required]],
+    cpfOuCnpj: ['45633314816',[Validators.required,Validators.minLength(11),Validators.maxLength(14)]],
+    senha: ['1237',[Validators.required]],
+    logradouro: ['Rua das Flores ',[Validators.required]],
+    numero: ['356',[]],
     complemento: ['',[]],
-    bairro: ['',[]],
-    cep: ['',[Validators.required]],
-    telefone1: ['',[Validators.required]],
+    bairro: ['Av. Souza Lopes',[]],
+    cep: ['08226015',[Validators.required]],
+    telefone1: ['65141844',[Validators.required]],
     telefone2: ['', []],
     telefone3: ['',[]],
     estadoId: [null,[Validators.required]],
@@ -68,6 +71,26 @@ export class SingupPage {
   }
 
   SingupUser(){
-    console.log("Envio o form");
+    this.clienteService.insert(this.formGroup.value)
+    .subscribe( response => {
+    this.showInsertOk();
+    },
+    error => {});
+  }
+  showInsertOk(){
+    let alert = this.alertCtrl.create( {
+      title: 'Sucesso',
+      message: 'Cadastro efetuado com sucesso',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            this.navCtrl.popTo('HomePage');
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
