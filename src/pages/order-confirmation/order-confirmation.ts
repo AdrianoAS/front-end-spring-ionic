6c6cb437d929e350f34b240850126d69f789e8f8
Adriano Araujo <aadriano990@gmail.com>
2018-10-06 16:16:42 -0300

    Informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -26,6 +26,7 @@ export class OrderConfirmationPage {
   cartItems : CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido: string;
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
@@ -57,15 +58,22 @@ export class OrderConfirmationPage {
   back(){
     this.navCtrl.setRoot('CartPage');
   }
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
   checkout(){
     this.pedidoService.insert(this.pedido)
     .subscribe(response => {
       this.cartService.creatOrClearCart();
-      console.log(response.headers.get('location'));
+      this.codPedido = this.extractId(response.headers.get('location'));
     },error => {
       if(error.status == 403){
         this.navCtrl.setRoot('HomePage');
       }
     });
+  }
+  private extractId(location: string ) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1 , location.length);
   }
 }
